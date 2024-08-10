d8c7c7ca3f0523139c53b2d7b3fb22fdd061249a
roja-pipeline-automation <ROJA-Pipeline-Automation@ibm.com>
2024-08-10 04:03:24 -0700

    Add ibm-concert-manage.sh

--- Software/manage/ibm-concert-manage.sh
+++ Software/manage/ibm-concert-manage.sh
@@ -5,53 +5,18 @@ scriptdir=`dirname $0`
 cd ${scriptdir}
 scriptdir=`pwd`
 dockerexe=${DOCKER_EXE:-podman}
-docker_image=${UTILS_IMG:-"icr.io/cpopen/ibm-aaf-utils:1.0.0"}
+docker_image=${UTILS_IMG:-"icr.io/cpopen/ibm-aaf-utils:1.0.1"}
 work_dir=${WORK_DIR:-"${scriptdir}/.ibm-concert-manage-utils"}
 
 container_name=ibm-aaf-utils
-tools_container_name=ibm-concert-toolbox
-release="4.8.3" # AAF Release
-components=${COMPONENTS:-"cpd_platform,concert"} 
+release=${RELEASE:-"5.0.0"} # AAF Release
+components=${COMPONENTS:-"concert"} 
 service_name=concert
-service_version=${SERVICE_VERSION:-"1.0.0"} # Concert Release
+service_version=${SERVICE_VERSION:-"1.0.1"} # Concert Release
 preview=${PREVIEW:-"false"}
 action=${ACTION:-"install"}
-
-
-# Write environment variables to <work_dir>/ibm-concert-manage.env
-function create_env_file {
-  # Remove ibm-concert-manage.env file if it exists
-  if test -f ${work_dir}/ibm-concert-manage.env; then
-      rm -f ${work_dir}/ibm-concert-manage.env
-  fi
-  echo "export DOCKER_EXE=${DOCKER_EXE:-podman}" >> ${work_dir}/ibm-concert-manage.env
-  echo "export UTILS_IMG=${UTILS_IMG:-"cp.icr.io/cpopen/concert/aaf-utils:latest"}" >> ${work_dir}/ibm-concert-manage.env
-  echo "export WORK_DIR=${WORK_DIR:-"${scriptdir}/.ibm-concert-manage-utils"}" >> ${work_dir}/ibm-concert-manage.env
-  echo "export container_name=ibm-aaf-utils" >> ${work_dir}/ibm-concert-manage.env
-  echo "export COMPONENTS=${COMPONENTS:-"cpd_platform,concert"}" >> ${work_dir}/ibm-concert-manage.env 
-  echo "export PREVIEW=${PREVIEW:-"false"}" >> ${work_dir}/ibm-concert-manage.env 
-  echo "export ACTION=${ACTION:-"install"}" >> ${work_dir}/ibm-concert-manage.env 
-  echo "export SERVICE_VERSION=${SERVICE_VERSION:-"1.0.0"}" >> ${work_dir}/ibm-concert-manage.env 
-  echo "export OCP_URL=<enter your Red Hat OpenShift Container Platform URL>" >> ${work_dir}/ibm-concert-manage.env
-  echo "export OPENSHIFT_TYPE=<enter your deployment type>" >> ${work_dir}/ibm-concert-manage.env
-  echo "export IMAGE_ARCH=<enter your cluster architecture>" >> ${work_dir}/ibm-concert-manage.env
-  echo "export PROJECT_OPERATOR=<enter your IBM Automation operator installation project>" >> ${work_dir}/ibm-concert-manage.env
-  echo "export PROJECT_INSTANCE=<enter your IBM Concert installation project>" >> ${work_dir}/ibm-concert-manage.env
-  echo "# export PROJECT_TETHERED=<enter the tethered project>" >> ${work_dir}/ibm-concert-manage.env
-  echo "export STG_CLASS_BLOCK=<RWO-storage-class-name>" >> ${work_dir}/ibm-concert-manage.env
-  echo "export STG_CLASS_FILE=<RWX-storage-class-name>" >> ${work_dir}/ibm-concert-manage.env
-  echo "# export IBM_ENTITLEMENT_KEY=<enter your IBM entitlement API key>" >> ${work_dir}/ibm-concert-manage.env
-  echo "# export PRIVATE_REGISTRY_LOCATION=<enter the location of your private container registry>" >> ${work_dir}/ibm-concert-manage.env
-  echo "# export PRIVATE_REGISTRY_PUSH_USER=<enter the username of a user that can push to the registry>" >> ${work_dir}/ibm-concert-manage.env
-  echo "# export PRIVATE_REGISTRY_PUSH_PASSWORD=<enter the password of the user that can push to the registry>" >> ${work_dir}/ibm-concert-manage.env
-  echo "# export PRIVATE_REGISTRY_PULL_USER=<enter the username of a user that can pull from the registry>" >> ${work_dir}/ibm-concert-manage.env
-  echo "# export PRIVATE_REGISTRY_PULL_PASSWORD=<enter the password of the user that can pull from the registry>" >> ${work_dir}/ibm-concert-manage.env
-  # for airgap install
-  echo "# export PRIVATE_IMAGE_REGISTRY_LOCATION=<enter the location where images are hosted>" >> ${work_dir}/ibm-concert-manage.env
-  echo "# export PRIVATE_IMAGE_REGISTRY_PULL_USER=<enter the username of a user that can pull images from the image registry>" >> ${work_dir}/ibm-concert-manage.env
-  echo "# export PRIVATE_IMAGE_REGISTRY_PULL_PASSWORD=<enter the password of the user that can pull the images from the image registry>" >> ${work_dir}/ibm-concert-manage.env
-
-}
+registry_location=${PRIVATE_REGISTRY_LOCATION:-"cp.icr.io"}
+registry_location="${registry_location}/cp/concert"
 
 # Function to execute a command
 function execute_command {
@@ -71,7 +36,6 @@ function initialize {
     imageAndTag=($(echo $docker_image | tr ":" "\n"))
 
     ${dockerexe} rm --force ${container_name} 2>/dev/null
-    ${dockerexe} rm --force ${tools_container_name} 2>/dev/null
     
     utilsImageIds=($(${dockerexe} images --sort "created" | grep ${container_name} | awk '{print $3}'))
     utilsImageTags=($(${dockerexe} images --sort "created" | grep ${container_name} | awk '{print $2}'))
@@ -82,23 +46,20 @@ function initialize {
         fi
     done
 
-    toolsImageTags=($(${dockerexe} images --sort "created" | grep ${tools_container_name} | awk '{print $2}'))
-    toolsImageIds=($(${dockerexe} images --sort "created" | grep ${tools_container_name} | awk '{print $3}'))
-    for i in ${!toolsImageTags[@]}; do
-        if [ "${imageAndTag[1]}" != "${toolsImageTags[$i]}" ]
-        then
-        ${dockerexe} rmi --force "${toolsImageIds[$i]}"
-        fi
-    done
-
     set -e
     echo ===== work_dir is ${work_dir} =====
     mkdir -p ${work_dir}
     chmod 777 ${work_dir}
-    ${dockerexe} run --name ${container_name} -v "${work_dir}":/tmp/work:z -d ${docker_image}
+    export DOCKER_DEFAULT_PLATFORM="linux/amd64"
+    if [ "${dockerexe}" == "podman" ]
+    then
+        ${dockerexe} run --arch=amd64 --name ${container_name} -v "${work_dir}":/tmp/work:z -d ${docker_image}
+    else
+        ${dockerexe} run --name ${container_name} -v "${work_dir}":/tmp/work:z -d ${docker_image}
+    fi
+    
     ${dockerexe} logs ${container_name}
     printf "to check on things:- \n ${dockerexe} exec -ti ${container_name} bash   \n   "
-    create_env_file
 }
 
 # Function to login-to-ocp
@@ -108,13 +69,24 @@ function login-to-ocp {
     ${dockerexe} exec $container_name oc project
 }
 
-# Function to install
+# Function to install concert
 function install {
     echo "install.."
     echo "apply-olm"
-    ${dockerexe} exec $container_name /opt/ansible/bin/olm-apply --release=${release} --case_download=true --components=${components} --cpd_operator_ns=${PROJECT_OPERATOR} -vvv --param-file=/tmp/work/param.yaml   # param file will be removed before release
+    ${dockerexe} exec $container_name /opt/ansible/bin/olm-apply --release=${release} --case_download=true --components=${components} --cpd_operator_ns=${PROJECT_OPERATOR}
+    echo "create-service-config"
+    ${dockerexe} exec $container_name create-service-config --service_name=${service_name} --service_version=${service_version} --operator_ns=${PROJECT_OPERATOR} --registry_location=${$registry_location}
     echo "apply-cr"
-    ${dockerexe} exec $container_name /opt/ansible/bin/cr-apply --release=${release} --components=${components} --cpd_operator_ns=${PROJECT_OPERATOR} --cpd_instance_ns=${PROJECT_INSTANCE} --block_storage_class=${STG_CLASS_BLOCK} --file_storage_class=${STG_CLASS_FILE} "$@"  # Scale config?
+    ${dockerexe} exec $container_name /opt/ansible/bin/cr-apply --release=${release} --components=${components} --cpd_operator_ns=${PROJECT_OPERATOR} --cpd_instance_ns=${PROJECT_INSTANCE} --block_storage_class=${STG_CLASS_BLOCK} --file_storage_class=${STG_CLASS_FILE} "$@" 
+}
+
+# Function to install zen 
+function install_zen {
+    echo "install zen.."
+    echo "apply-aaf-zen"
+    ${dockerexe} exec $container_name apply_aaf_zen --operator_ns=${PROJECT_OPERATOR} --instance_ns=${PROJECT_INSTANCE}
+    echo "apply-aaf-zen-cr"
+    ${dockerexe} exec $container_name apply-aaf-zen-cr --instance_ns=${PROJECT_INSTANCE} --block_storage_class=${STG_CLASS_BLOCK} --service_name=${service_name} 
 }
 
 # Function to check status
@@ -149,16 +121,9 @@ function get-cr-status {
     ${dockerexe} exec $container_name /opt/ansible/bin/get-cr-status --cpd_instance_ns=${PROJECT_INSTANCE}
 }
 
-# get cpd login credentials
-function get-cpd-instance-details {
-    echo "get-cpd-instance-details"
-    ${dockerexe} exec $container_name /opt/ansible/bin/get-cpd-instance-details --cpd_instance_ns=${PROJECT_INSTANCE} --get_admin_initial_credentials=true
-}
-
 function get-concert-instance-details {
     echo "get-concert-instance-details"
-    cpd_creds=`${dockerexe} exec $container_name /opt/ansible/bin/get-cpd-instance-details --cpd_instance_ns=${PROJECT_INSTANCE} --get_admin_initial_credentials=true`
-    echo $cpd_creds | sed 's/CPD/Concert/g'
+    ${dockerexe} exec $container_name get-concert-instance-details --instance_ns=${PROJECT_INSTANCE} --get_admin_initial_credentials=true
 }
 
 function apply-cluster-components {
@@ -172,8 +137,8 @@ function authorize-instance-topology {
 }
 
 function setup-instance-topology {
-    echo "authorize-instance-topology"
-    ${dockerexe} exec $container_name /opt/ansible/bin/setup-instance-topology --release=${release} --cpd_operator_ns=${PROJECT_OPERATOR} --cpd_instance_ns=${PROJECT_INSTANCE} --license_acceptance=true 
+    echo "setup-instance-topology"
+    ${dockerexe} exec $container_name /opt/ansible/bin/setup-instance-topology --release=${release} --cpd_operator_ns=${PROJECT_OPERATOR} --cpd_instance_ns=${PROJECT_INSTANCE} --license_acceptance=true --block_storage_class=${STG_CLASS_BLOCK}
 }
 
 function delete-cr {
@@ -186,6 +151,11 @@ function delete-olm-artifacts {
     echo "delete-olm-artifacts"
     ${dockerexe} exec $container_name /opt/ansible/bin/delete-olm-artifacts --cpd_operator_ns=${PROJECT_OPERATOR} --components=${components}
 
+}
+
+function login-entitled-registry {
+    echo "login-entitled-registry"
+    ${dockerexe} exec $container_name /opt/ansible/bin/login-entitled-registry ${IBM_ENTITLEMENT_KEY}
 }
 
 function login-private-registry {
@@ -213,15 +183,19 @@ function mirror-images {
     ${dockerexe} exec $container_name /opt/ansible/bin/mirror-images --components=${components} --release=${release} --target_registry=${PRIVATE_REGISTRY_LOCATION} --case_download=false
 }
 
-function create-service-config {
-    echo "create-service-config"
-    ${dockerexe} exec $container_name create-service-config  --service_name=${service_name} --service_version=${service_version} --operator_ns=${PROJECT_OPERATOR} --registry_location=${PRIVATE_REGISTRY_LOCATION}
+function apply-aaf-icsp {
+    echo "apply-aaf-icsp"
+    ${dockerexe} exec $container_name apply-aaf-icsp --registry=${PRIVATE_REGISTRY_LOCATION}
 }
 
+function create-service-config {
+    echo "create-service-config"
+    ${dockerexe} exec $container_name create-service-config  --service_name=${service_name} --service_version=${service_version} --operator_ns=${PROJECT_OPERATOR} --registry_location=${registry_location}
+}
 
 function concert-setup {
     echo "concert-setup"
-    ${dockerexe} exec $container_name concert-setup --action=${action} --release=${release} --license_acceptance=true --operator_ns=${PROJECT_OPERATOR} --operand_ns=${PROJECT_INSTANCE} --preview=${preview} --components=${components} --service_name=${service_name} --service_version=${service_version} --block_storage_class=${STG_CLASS_BLOCK} --file_storage_class=${STG_CLASS_FILE} --registry_location=${PRIVATE_REGISTRY_LOCATION}
+    ${dockerexe} exec $container_name concert-setup --action=${action} --release=${release} --license_acceptance=true --operator_ns=${PROJECT_OPERATOR} --operand_ns=${PROJECT_INSTANCE} --preview=${preview} --components=${components} --service_name=${service_name} --service_version=${service_version} --block_storage_class=${STG_CLASS_BLOCK} --file_storage_class=${STG_CLASS_FILE} --registry_location=${registry_location}
 }
 
 function print-help {
@@ -243,6 +217,9 @@ case "$1" in
     install)
         install "${@:2}"
         ;;
+    install_zen)
+        install_zen
+        ;;
     status)
         status "${@:2}"
         ;;
@@ -260,9 +237,6 @@ case "$1" in
         ;;
     get-cr-status)
         get-cr-status
-        ;;
-    get-cpd-instance-details)
-        get-cpd-instance-details
         ;;
     apply-cluster-components)
         apply-cluster-components
@@ -282,6 +256,9 @@ case "$1" in
     login-private-registry)
         login-private-registry
         ;;
+    login-entitled-registry)
+        login-entitled-registry
+        ;;
     list-images)
         list-images
         ;;
@@ -293,6 +270,9 @@ case "$1" in
         ;;
     list-images-mirrored)
         list-images-mirrored
+        ;;
+    apply-aaf-icsp)
+        apply-aaf-icsp
         ;;
     create-service-config)
         create-service-config
