32200fb67e7696517dee60ac1f91ae04ac92e7d9
Roja-Pipeline-Automation <Roja-Pipeline-Automation@ibm.com>
2025-11-04 18:27:32 -0800

    sync with release-1.1.0.2 updates

--- Software/backup-restore/ibm-concert-restore.sh
+++ Software/backup-restore/ibm-concert-restore.sh
@@ -1,12 +1,51 @@
 #!/bin/bash
 
-#Get registry & image tag from configmap
-IMG_PREFIX=$(kubectl get cm app-build-cfg-cm -n ${INSTANCE_NAMESPACE} -o jsonpath='{.data.IMG_PREFIX}')
-IMG_TAG=$(kubectl get cm app-build-cfg-cm -n ${INSTANCE_NAMESPACE} -o jsonpath='{.data.IMG_TAG}')
+
+print_usage()
+{
+ cat << EOM
+    ibm-concert-restore
+    -----
+    Perfrom the pre-steps by setting up a PVC called concert-backup-restore-pvc and create a secret backup-secret of the bucket where the data is stored.
+    For more details, please refer the Concert documentation.  
+    To take the concert backup run ./ibm-concert-backup.sh after exporting below values:
+    - --instance_ns
+    - --backup_version
+    - --backup_timestamp
+    - --backup_entity
+    Instead of passing the values as command line parameters, you could also export variables to run the script as listed below:
+
+      export INSTANCE_NAMESPACE=<your-namespace>
+      export BACKUP_ENTITY=>configdb,appdb,EL or LZ>
+      export BACKUP_VERSION=<version of Concert>
+      export BACKUP_TIMESTAMP=<timestamp of the backup you want to restore>
+
+      Example:
+      export INSTANCE_NAMESPACE=<your-namespace>
+      export BACKUP_ENTITY=appdb
+      export BACKUP_VERSION=v1.0.5-199-20250111.044001-main
+      export BACKUP_TIMESTAMP=20250210_194423
+
+    Run the below restore script 
+    ./ibm-concert-restore.sh
+
+    The sample output looks like this below: 
+      ... Restoring concert backup ..
+      job.batch/concert-restore created
+
+EOM
+}
+
+
 
 validate_inputs()
 {
     while [ $# -gt 0 ] ; do
+      if [[ "$1" == --help ]]
+      then
+        print_usage
+        exit 0
+      fi
       if [[ "$1" == --instance_ns=* ]]
       then
           INSTANCE_NAMESPACE=$(echo $1 | cut -f2 -d=) ;
@@ -16,10 +55,13 @@ validate_inputs()
       then
           BACKUP_ENTITY=$(echo $1 | cut -f2 -d=) ;
       fi
-
-      if [[ "$1" == --backup_location=* ]]
+      if [[ "$1" == --backup_version=* ]]
       then
-          BACKUP_LOCATION=$(echo $1 | cut -f2 -d=) ;
+          BACKUP_VERSION=$(echo $1 | cut -f2 -d=) ;
+      fi
+      if [[ "$1" == --backup_timestamp=* ]]
+      then
+          BACKUP_TIMESTAMP=$(echo $1 | cut -f2 -d=) ;
       fi
 
       shift
@@ -52,6 +94,34 @@ validate_inputs()
 
 validate_inputs "$@"
 
+#Get registry & image tag from configmap
+IMG_PREFIX=$(kubectl get cm app-build-cfg-cm -n ${INSTANCE_NAMESPACE} -o jsonpath='{.data.IMG_PREFIX}')
+IMG_TAG=$(kubectl get cm app-build-cfg-cm -n ${INSTANCE_NAMESPACE} -o jsonpath='{.data.IMG_TAG}')
+
+RUNASUSER=${USER_ID:-$(kubectl get cm app-cfg-cm -n ${INSTANCE_NAMESPACE} -o jsonpath='{.data.RUNASUSER}')}
+RUNASGROUP=${GROUP_ID:-$(kubectl get cm app-cfg-cm -n ${INSTANCE_NAMESPACE} -o jsonpath='{.data.RUNASGROUP}')}
+FS_GROUPID=${FS_GROUPID:-$(kubectl get cm app-cfg-cm -n ${INSTANCE_NAMESPACE} -o jsonpath='{.data.FS_GROUPID}')}
+SUPP_GROUPID=${SUPP_GROUPID:-$(kubectl get cm app-cfg-cm -n ${INSTANCE_NAMESPACE} -o jsonpath='{.data.SUPP_GROUPID}')}
+if [ "X$RUNASUSER" == "X" ];
+then
+  echo "Please enter USER_ID"
+  exit 1
+fi
+if [ "X$RUNASGROUP" == "X" ];
+then
+  echo "Please enter GROUP_ID"
+  exit 1
+fi
+if [ "X$FS_GROUPID" == "X" ];
+then
+  echo "Please enter FS_GROUPID"
+  exit 1
+fi
+if [ "X$SUPP_GROUPID" == "X" ];
+then
+  echo "Please enter SUPP_GROUPID"
+  exit 1
+fi
 
 ConcertRestore()
 
@@ -69,6 +139,14 @@ spec:
   suspend: false
   template:
     spec:
+      serviceAccount: app-sa
+      serviceAccountName: app-sa
+      securityContext:
+        runAsNonRoot: true
+        fsGroupChangePolicy: OnRootMismatch
+        fsGroup: ${FS_GROUPID}
+        supplementalGroups:
+        - ${SUPP_GROUPID}
       restartPolicy: OnFailure
       volumes:
         - name: concert-backup-restore
@@ -102,6 +180,8 @@ spec:
           secret:
             secretName: app-cfg-internal-tls
             defaultMode: 420
+        - name: tmp
+          emptyDir: {}
       containers:
         - resources:
             limits:
@@ -120,6 +200,9 @@ spec:
             capabilities:
               drop:
                 - ALL
+            readOnlyRootFilesystem: true
+            runAsUser: ${RUNASUSER}
+            runAsGroup: ${RUNASGROUP}
           imagePullPolicy: IfNotPresent
           volumeMounts:
             - name: concert-backup-restore
@@ -132,6 +215,8 @@ spec:
               mountPath: /app/tmp/self-signed-ssl
             - name: backup-secret
               mountPath: /mnt/infra/backup-secret
+            - name: tmp
+              mountPath: /tmp
           terminationMessagePolicy: File
           image: "${IMG_PREFIX}/ibm-roja-py-utils:${IMG_TAG}"
           args: ["/app/bin/backup-restore/restore-from-bucket.sh ${BACKUP_ENTITY} ${BACKUP_VERSION} ${BACKUP_TIMESTAMP}"]
